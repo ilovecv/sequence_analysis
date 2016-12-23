65cadb6416d588a13bc519be9a1c15cb93592d70
behrooz <behrooz.mahasseni@gmail.com>
2016-12-23 14:07:53 -0800

    add confusion matrix plot

--- src/sequence_eval.py
+++ src/sequence_eval.py
@@ -80,6 +80,8 @@ def eval(FLAGS):
     confusion_matrix = np.zeros((FLAGS.output_size, FLAGS.output_size), dtype=np.int32)
     entire_gt = []
     entire_es = []
+    total = 0.0
+    correct = 0.0
     with sess:
         saver.restore(sess, FLAGS.model_path)
         for i in xrange(FLAGS.number_of_test_examples):
@@ -91,15 +93,17 @@ def eval(FLAGS):
                 confusion_matrix[ground_truth[j][-1], estimated_labels[j]] += 1
                 entire_gt.append(ground_truth[j][-1])
                 entire_es.append(estimated_labels[j])
-                local_confusion_matrix[ground_truth[j], estimated_labels[j]] += 1            
-            # print('local_confusion_matrix', local_confusion_matrix)
+                if ground_truth[j][-1] == estimated_labels[j]:
+                    correct += 1
+            total += len(ground_truth)    
     print('confusion_matrix', confusion_matrix)
+    print ('accuracy:' + correct / total)
     entire_gt = np.asanyarray(entire_gt, np.int32)
     entire_es = np.asanyarray(entire_es, np.int32)
     cnf_matrix = compute_confusion_matrix(entire_gt, entire_es)
     
     plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=classes, normalize=True,
+    plot_confusion_matrix(cnf_matrix, classes=[], normalize=True,
                       title='Normalized confusion matrix')
 
     plt.show()
