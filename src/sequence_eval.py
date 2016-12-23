589db9c8851141904820c10e02a1d455b407d00d
behrooz <behrooz.mahasseni@gmail.com>
2016-12-23 14:14:47 -0800

    add confusion matrix plot

--- src/sequence_eval.py
+++ src/sequence_eval.py
@@ -15,39 +15,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix as compute_confusion_matrix
 
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-    """
-    This function prints and plots the confusion matrix.
-    Normalization can be applied by setting `normalize=True`.
-    """
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
-
-    print(cm)
-
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
 
 def eval(FLAGS):
     classes_file = open('ucf_train_test_files/classInd.txt')
@@ -55,7 +22,6 @@ def eval(FLAGS):
     for line in classes_file:
         s = line.split(' ')
         classes.append(s.strip)
-    print classes
     sequence_reader = Sequence_Reader(FLAGS)
     if FLAGS.sequence_classifer_type == 1:
         sequence_classifier = Sequence_Classifier(FLAGS)
@@ -99,11 +65,4 @@ def eval(FLAGS):
     print('confusion_matrix', confusion_matrix)
     print ('accuracy:' + str(correct / total))
     entire_gt = np.asanyarray(entire_gt, np.int32)
-    entire_es = np.asanyarray(entire_es, np.int32)
-    cnf_matrix = compute_confusion_matrix(entire_gt, entire_es)
-    
-    plt.figure()
-    plot_confusion_matrix(cnf_matrix, classes=[], normalize=True,
-                      title='Normalized confusion matrix')
-
-    plt.show()
+    entire_es = np.asanyarray(entire_es, np.int32)    
