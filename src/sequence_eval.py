14804d23cc5614056dbc85a99d115e4af9ca6623
behrooz <behrooz.mahasseni@gmail.com>
2016-12-23 13:54:43 -0800

    add confusion matrix plot

--- src/sequence_eval.py
+++ src/sequence_eval.py
@@ -3,6 +3,7 @@ Created on Oct, 2016
 
 @author: mitra
 '''
+import itertools
 import os
 import tensorflow as tf
 from sequence_reader import Sequence_Reader
@@ -11,10 +12,50 @@ from sequence_model import Sequence_Classifier_With_Convolution
 import time
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 def eval(FLAGS):
-    
+    classes_file = open('ucf_train_test_files/classInd.txt')
+    classes = []
+    for line in classes_file:
+        s = line.split(' ')
+        classes.append(s.strip)
+    print classes
     sequence_reader = Sequence_Reader(FLAGS)
     if FLAGS.sequence_classifer_type == 1:
         sequence_classifier = Sequence_Classifier(FLAGS)
@@ -37,17 +78,28 @@ def eval(FLAGS):
     tf.train.start_queue_runners(sess=sess)
 
     confusion_matrix = np.zeros((FLAGS.output_size, FLAGS.output_size), dtype=np.int32)
+    entire_gt = []
+    entire_es = []
     with sess:
         saver.restore(sess, FLAGS.model_path)
-        for i in xrange(1000):
+        for i in xrange(FLAGS.number_of_test_examples):
             estimated_labels, ground_truth = sess.run(test_op)
             estimated_labels = np.argmax(estimated_labels, axis=1)
             local_confusion_matrix = np.zeros((FLAGS.output_size, FLAGS.output_size), dtype=np.int32)
             ground_truth = np.squeeze(ground_truth)
-            
             for j in xrange(len(ground_truth)):                
                 confusion_matrix[ground_truth[j][-1], estimated_labels[j]] += 1
+                entire_gt.append(ground_truth[j][-1])
+                entire_es.append(estimated_labels[j])
                 local_confusion_matrix[ground_truth[j], estimated_labels[j]] += 1            
             # print('local_confusion_matrix', local_confusion_matrix)
     print('confusion_matrix', confusion_matrix)
-            
+    entire_gt = np.asanyarray(entire_gt, np.int32)
+    entire_es = np.asanyarray(entire_es, np.int32)
+    cnf_matrix = confusion_matrix(entire_gt, entire_es)
+    
+    plt.figure()
+    plot_confusion_matrix(cnf_matrix, classes=classes, normalize=True,
+                      title='Normalized confusion matrix')
+
+    plt.show()
